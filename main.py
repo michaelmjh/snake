5435a79e225279cc43d4249e922fa9def4aa9075
michaelmjh <michaelmjh@icloud.com>
2021-06-26 22:18:40 +0100

    Update

--- main.py
+++ main.py
@@ -1,1 +1,40 @@
-print("hello")
+from turtle import Turtle, Screen
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake")
+screen.tracer(0)
+
+snake = []
+x_start = 0
+
+for segment in range(0, 3):
+    segment = Turtle()
+    segment.shape("square")
+    segment.color("white")
+    segment.penup()
+    segment.setpos(x_start, 0)
+    snake.append(segment)
+    x_start -= 20
+
+
+
+game_on = True
+while game_on:
+    screen.update()
+    time.sleep(0.1)
+    for segment in snake:
+        segment.forward(20)
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
